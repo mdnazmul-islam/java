b895c643772e587cce6a4620fc7a60af45f6c1e4
Nazmul islam <155037365+mdnazmul-islam@users.noreply.github.com>
2025-01-07 18:45:23 +0600

    Update arithmetic.java

--- Anisul_Islam_video/video/class 15/arithmetic.java
+++ Anisul_Islam_video/video/class 15/arithmetic.java
@@ -5,6 +5,6 @@ class Arithmetic
         int number1=23;
         int number2 = 35;
         int sum =number1+number2;
-        System.out.println("The sum="+sum);
+        System.out.println("The sum="+sum); 
     }
 }
