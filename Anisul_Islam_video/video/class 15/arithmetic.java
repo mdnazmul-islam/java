db0465050c8b53a9cafcb5f4a97d17284fbedf95
Nazmul islam <155037365+mdnazmul-islam@users.noreply.github.com>
2025-01-07 18:44:01 +0600

    two input

--- Anisul_Islam_video/video/class 15/arithmetic.java
+++ Anisul_Islam_video/video/class 15/arithmetic.java
@@ -1,1 +1,10 @@
-
+class Arithmetic
+{
+    public static void main(String[] args)
+    {
+        int number1=23;
+        int number2 = 35;
+        int sum =number1+number2;
+        System.out.println("The sum="+sum);
+    }
+}
