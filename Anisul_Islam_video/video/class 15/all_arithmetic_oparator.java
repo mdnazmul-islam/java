2bd25ef92b193b67a55547004a2cefb7d97a0156
Nazmul islam <155037365+mdnazmul-islam@users.noreply.github.com>
2025-01-07 18:54:51 +0600

    +,-,*,/ all oparator

---
+++ Anisul_Islam_video/video/class 15/all_arithmetic_oparator.java
@@ -0,0 +1,17 @@
+class Arithmetic
+{
+    public static void main(String[] args)
+    {
+        int number1=23;
+        int number2 = 35;
+        int sum =number1+number2;
+        System.out.println("The sum="+sum);
+        int sub=number1-number2;
+        System.out.println("The subtractor ="+sub);
+        int multiplication =number1*number2;
+        System.out.println("the multiplication ="+multiplication);
+        int divaided = number1/number2;
+        System.out.println("The divaided ="+ divaided);
+        
+    }
+}
