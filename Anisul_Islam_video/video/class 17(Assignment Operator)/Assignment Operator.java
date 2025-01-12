30590438a31c7b4fd77ddb6d002b2f2d497a3f0b
Nazmul islam <155037365+mdnazmul-islam@users.noreply.github.com>
2025-01-12 14:56:41 +0600

    =,+=,-=,*=,/=,%=

---
+++ Anisul_Islam_video/video/class 17(Assignment Operator)/Assignment Operator.java
@@ -0,0 +1,19 @@
+class Assignment
+{
+   public static void main(String[]args)
+   {
+     int x = 10;
+     int y = 5;
+     x+=y;
+     System.out.println("x += y = "+x);
+     x-=y;
+     System.out.println("x -= y = "+x);
+      x*=y;
+     System.out.println("x *= y = "+x); 
+     x/=y;
+     System.out.println("x /= y = "+x); 
+     x%=y;
+     System.out.println("x %= y = "+x);
+      
+   }
+}
