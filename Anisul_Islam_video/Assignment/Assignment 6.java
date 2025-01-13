1b388e4c0afe5f684b0d9c5c99f2718a016cadde
Nazmul islam <155037365+mdnazmul-islam@users.noreply.github.com>
2025-01-13 16:57:02 +0600

    Create Assignment 6.java

---
+++ Anisul_Islam_video/Assignment/Assignment 6.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+class dighit_spelling
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the one digite 0-9 :");
+        int Digite = input.nextInt();
+        if(Digite==0)
+        {
+            System.out.println("Zero");
+        }
+        if(Digite==1)
+        {
+            System.out.println("One");
+        }
+        if(Digite==2)
+        {
+            System.out.println("Two");
+        }
+        if(Digite==3)
+        {
+            System.out.println("Three");
+        }
+        if(Digite==4)
+        {
+            System.out.println("Four");
+        }
+        if(Digite==5)
+        {
+            System.out.println("Five");
+        }
+        if(Digite==6)
+        {
+            System.out.println("Six");
+        }
+        if(Digite==7)
+        {
+            System.out.println("Seven");
+        }
+        if(Digite==8)
+        {
+            System.out.println("Eight");
+        }
+        if(Digite==9)
+        {
+            System.out.println("Nine");
+        }
+        
+    }
+}
