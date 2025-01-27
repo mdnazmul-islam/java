9d3f6c498fefc226a2923b787811083c5c6d884a
Nazmul islam <155037365+mdnazmul-islam@users.noreply.github.com>
2025-01-27 19:25:23 +0600

    Create switch.java

---
+++ Anisul_Islam_video/video/Class 31(switch)/switch.java
@@ -0,0 +1,42 @@
+import java.util.*;
+class Switch
+{
+    public static void main(String[] args)
+    {
+        int digite;
+        System.out.print("Enter the digit:");
+        Scanner input= new Scanner(System.in);
+        digite=input.nextInt();
+        switch(digite)
+        {
+            case 1:
+            {
+                System.out.println("One");
+                 break;
+            }
+            case 2:
+            {
+                System.out.println("Two");
+                break;
+            }
+            case 3:
+            {
+                System.out.println("Two");
+                break;
+            }
+            case 5:
+            {
+                System.out.println("Two");
+                break;
+            }
+            default:
+            {
+                System.out.println("NOt answer.");
+            }
+            
+
+        }
+
+    }
+    
+}
