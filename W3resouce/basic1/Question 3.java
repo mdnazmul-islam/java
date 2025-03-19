8e6cbc5737a389dd1de14eca9b27bc36ed27f47a
Nazmul islam <155037365+mdnazmul-islam@users.noreply.github.com>
2025-03-19 18:52:20 +0600

    Division of Two Numbers

---
+++ W3resouce/basic1/Question 3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Question3{
+    public static void main(String[]args)
+    {
+        Scanner input =new Scanner(System.in);
+        System.out.println("Enter the two number:");
+        Double number1=input.nextDouble();
+        Double number2=input.nextDouble ();
+        System.out.println("Two number division :"+(number1/number2));
+    }
+}
