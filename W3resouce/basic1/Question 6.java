0f44c40015da96ad96c7fc1847a039a9ee696193
Nazmul islam <155037365+mdnazmul-islam@users.noreply.github.com>
2025-03-19 19:16:02 +0600

    Basic Arithmetic Operations

---
+++ W3resouce/basic1/Question 6.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Question5{
+    public static void main(String[]args)
+    {
+       Scanner input =new Scanner(System.in);
+       System.out.print("input first number:");
+       int number1=input.nextInt();
+       System.out.print("Input second number:");
+       int number2=input.nextInt();
+       System.out.println(number1+" + "+number2+" = "+(number1+number2));
+       System.out.println(number1+" - "+number2+" = "+(number1-number2));
+       System.out.println(number1+" x "+number2+" = "+(number1*number2));
+       System.out.println(number1+" / "+number2+" = "+(number1/number2));
+       System.out.println(number1+" mod "+number2+" = "+(number1%number2));
+    }
+}
