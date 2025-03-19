4b40b762eacba3ef51b9538625852b7cd0c5a399
Nazmul islam <155037365+mdnazmul-islam@users.noreply.github.com>
2025-03-19 23:36:35 +0600

    Expression Evaluation

---
+++ W3resouce/basic1/Question 9.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class Question9{
+    public static void main(String[]args)
+    {
+        System.out.println(((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)));
+    }
+   
+}
