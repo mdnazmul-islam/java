261a041f54a260908ad05c584029b8a8d3cae836
Nazmul islam <155037365+mdnazmul-islam@users.noreply.github.com>
2025-03-19 18:46:15 +0600

    Sum of Two Numbers

---
+++ W3resouce/basic1/Question 2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Question2{
+    public static void main(String[]args)
+    {
+        Scanner input =new Scanner(System.in);
+        System.out.println("Enter the two number:");
+        int number1=input.nextInt();
+        int number2=input.nextInt();
+        System.out.println("Two number sum :"+(number1+number2));
+    }
+}
