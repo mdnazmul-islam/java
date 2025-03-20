d3bf0146a0e6d2dffcbce08f048d3ee3e51d86f6
Nazmul islam <155037365+mdnazmul-islam@users.noreply.github.com>
2025-03-20 19:14:38 +0600

    odd number sum

---
+++ Anisul_Islam_video/video/class 046(sum)/odd number sum.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class class46{
+   
+    public static void main(String[] args) {
+       Scanner input =new Scanner(System.in);
+       System.out.print("start odd number:");
+       int start= input.nextInt();
+       System.out.print("End odd number:");
+       int end=input.nextInt();
+       int sum=0;
+       for (int i = start; i <=end ; i=i+2) {
+        sum+=i;
+        
+       }
+       System.out.println(start+" to "+end+" sum odd number :"+sum);
+    }
+   
+}
