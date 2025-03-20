cb0cc25bdb72135453b7e3d50d8d3c19eab957a5
Nazmul islam <155037365+mdnazmul-islam@users.noreply.github.com>
2025-03-20 11:08:32 +0600

    Circle: Area and Perimeter

---
+++ W3resouce/basic1/Question 011.java
@@ -0,0 +1,9 @@
+class Question11
+{
+    public static void main(String[] args) {
+        double Radius=7.5;
+        System.out.println("perimeter is = "+(2*3.1416*Radius));
+        System.out.println("Area is = "+(3.1416*Radius*Radius));
+
+    }
+}
