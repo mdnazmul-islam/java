60170e6f6ce714f345149a88a827d1cd7f1ec059
Nazmul islam <155037365+mdnazmul-islam@users.noreply.github.com>
2025-03-21 00:34:41 +0600

    start to end armstrong number

---
+++ Anisul_Islam_video/Assignment/Assignment 015.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class Class65
+{
+   
+    public static void main(String[] args) {
+       Scanner input =new Scanner(System.in);
+       System.out.print("srart armstrong number:");
+       int start = input.nextInt();
+       System.out.print("end armstrong number:");
+       int end = input.nextInt();
+       int count=0;
+      System.out.print(start+" to "+end+" armstrong number :");
+       int number=start;
+       while(number<=end){
+        int orginal=number,sum=0;
+        while(number!=0)
+        {
+          int remender= number%10;
+          sum=sum+(remender*remender*remender);
+          number=number/10;
+        }
+        if(orginal==sum)
+        {
+         System.out.print(" "+sum);
+         count++;
+        }
+       
+       number=orginal+1;
+       }
+      System.out.println("\ntotul armstrong number :"+count);
+      
+       
+    }
+   
+}
