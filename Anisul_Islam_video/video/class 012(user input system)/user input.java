91f285ea50cd298eb4326b20941440e713a99409
Nazmul islam <155037365+mdnazmul-islam@users.noreply.github.com>
2025-03-21 11:07:58 +0600

    user input system

--- Anisul_Islam_video/video/class 012(user input system)/user input.java
+++ Anisul_Islam_video/video/class 012(user input system)/user input.java
@@ -1,1 +1,16 @@
-
+import java.util.Scanner;
+class Input
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int number;
+        String name;
+        System.out.print("Enter any number:");
+        number = input.nextInt();
+        System.out.println("Enter the name:");
+        name=input.next();
+        System.out.println("the number ="+number);
+        System.out.println(name);
+    }
+}
